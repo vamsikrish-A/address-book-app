b429564a4553ffc7c3919b9a1ef2a9fe2ac8d573
vamsikrish-A <krishnavamsi165@gmail.com>
2021-12-17 01:58:25 +0530

    added:curd operations service methids for apis

---
+++ src/main/java/com/bridgelabz/addressbookapp/service/AddressBookService.java
@@ -0,0 +1,51 @@
+package com.bridgelabz.addressbookapp.service;
+
+import com.bridgelabz.addressbookapp.builder.AddressBookBuilder;
+import com.bridgelabz.addressbookapp.dto.AddressBookDto;
+import com.bridgelabz.addressbookapp.dto.ResponseDto;
+import com.bridgelabz.addressbookapp.entity.AddressBookEntity;
+import com.bridgelabz.addressbookapp.repository.AddressBookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AddressBookService {
+    private static final String ADDRESSBOOK_ADDED_SUCCESSFULLY = "DETAILS UPDATED SUCCESSFULLY";
+    private static final String ADDRESSBOOK_UPDATED_SUCCESSFULLY = "DETAILS UPDATED SUCCESSFULLY";
+
+    @Autowired
+    private AddressBookRepo addressBookRepo;
+
+    @Autowired
+    private AddressBookBuilder addressBookBuilder;
+
+    public List<AddressBookEntity> getAddressBookDetails() {
+        return addressBookRepo.findAll();
+    }
+
+    public AddressBookEntity getAddressBookDetailsById(int id) {
+        return addressBookRepo.findById(id).orElseThrow(null);
+    }
+
+    public ResponseDto addAddressBookDetails(AddressBookDto addressBookDto) {
+        AddressBookEntity addressBookEntity = new AddressBookEntity();
+        addressBookEntity = addressBookBuilder.buildAddressBookEntity(addressBookDto, addressBookEntity);
+        addressBookRepo.save(addressBookEntity);
+        return new ResponseDto(ADDRESSBOOK_ADDED_SUCCESSFULLY, HttpStatus.CREATED);
+    }
+
+    public ResponseDto updateAddressBookDetails(int id, AddressBookDto addressBookDto) {
+        AddressBookEntity addressBookEntity = getAddressBookDetailsById(id);
+        addressBookEntity = addressBookBuilder.buildAddressBookEntity(addressBookDto, addressBookEntity);
+        addressBookRepo.save(addressBookEntity);
+        return new ResponseDto(ADDRESSBOOK_UPDATED_SUCCESSFULLY, HttpStatus.ACCEPTED);
+    }
+
+    public ResponseDto deleteAddressBookDetails(int id) {
+        addressBookRepo.deleteById(getAddressBookDetailsById(id));
+        return new ResponseDto("AddressBook details with ID:" +id+" Deleted Successfully", HttpStatus.ACCEPTED);
+    }
+}
