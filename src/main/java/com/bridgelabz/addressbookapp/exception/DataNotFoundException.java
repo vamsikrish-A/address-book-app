61d05b13623aeb6f34e20815bbb403eea0407001
vamsikrish-A <krishnavamsi165@gmail.com>
2021-12-17 01:59:51 +0530

    Added:custom and global exception classes for http status and validation messages

---
+++ src/main/java/com/bridgelabz/addressbookapp/exception/DataNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbookapp.exception;
+
+public class DataNotFoundException extends com.bridgelabz.addressbookapp.exception.CustomException {
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
