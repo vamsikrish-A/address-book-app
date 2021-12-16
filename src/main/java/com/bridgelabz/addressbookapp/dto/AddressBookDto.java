69db0c58092a79ee3e034ee072bf054753b8b69b
vamsikrish-A <krishnavamsi165@gmail.com>
2021-12-17 01:54:44 +0530

    Added:dto and response dto which contains message and data objects

---
+++ src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDto.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.addressbookapp.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Data
+public class AddressBookDto {
+    @NotNull(message = "First name Should not be empty")
+    @Pattern(regexp = "^[A-Z][a-z]{2,}$", message = "First name is invalid!!!")
+    private String firstName;
+    @NotNull(message = "Lastname should not be empty")
+    @Pattern(regexp = "^[A-Z][a-z]{2,}$", message = "LastName is invalid!")
+    private String lastName;
+    @NotNull(message = "CityName should not be empty")
+    private String city;
+    @NotNull(message = "State name should not be empty")
+    private String state;
+    @NotNull(message = "phone  number should not be empty")
+    @Pattern(regexp = "^[0-9]{10,12}$",message = "phone number invalid")
+    private String phoneNumber;
+    @NotNull(message = "zipcode should not be empty")
+    @Size(max = 6, message = "Zipcode is invalid.")
+    private String zipCode;
+}
---
+++ src/main/java/com/bridgelabz/addressbookapp/dto/ResponseDto.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.addressbookapp.dto;
+
+import lombok.Data;
+
+@Data
+public class ResponseDto {
+    private String message;
+    private Object data;
+
+    public ResponseDto(String message, Object data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    public ResponseDto() {
+
+    }
+}
