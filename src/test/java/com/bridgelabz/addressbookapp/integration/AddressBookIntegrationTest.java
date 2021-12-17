b807816335ac8830eab02bbe5f18ba201c77a2cd
vamsikrish-A <krishnavamsi165@gmail.com>
2021-12-17 22:58:58 +0530

    Added:integration test cases for application using MockMVC

--- src/test/java/com/bridgelabz/addressbookapp/integration/AddressBookIntegrationTest.java
+++ src/test/java/com/bridgelabz/addressbookapp/integration/AddressBookIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbookapp.integration;
 
+import com.bridgelabz.addressbookapp.dto.AddressBookDto;
 import com.bridgelabz.addressbookapp.dto.ResponseDto;
 import com.bridgelabz.addressbookapp.entity.AddressBookEntity;
 import com.bridgelabz.addressbookapp.service.AddressBookService;
@@ -9,6 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -32,7 +34,7 @@ public class AddressBookIntegrationTest {
 
     @Test
     void addAddressbookDetailsTest() throws Exception {
-        when(addressBookService.addAddressBookDetails(any())).thenReturn( new ResponseDto());
+        when(addressBookService.addAddressBookDetails(any())).thenReturn( new ResponseDto("DETAILS ADDED SUCCESSFULLY", HttpStatus.CREATED));
         mockMvc.perform(MockMvcRequestBuilders.post("/addressbook/details")
                 .content("\"{\\\"firstName\\\": \\\"Vamsi\\\", +" +
                 "\\\"lastName\\\": \\\"Krishna\\\", \\\"city\\\": \\\"SriKalahasti\\\", +" +
@@ -52,6 +54,31 @@ public class AddressBookIntegrationTest {
         AddressBookEntity addressBookEntity = new AddressBookEntity();
         when(addressBookService.getAddressBookDetailsById(1)).thenReturn(addressBookEntity);
         mockMvc.perform(MockMvcRequestBuilders.get("/addressbook/details/1"))
+                .andExpect(status().is5xxServerError());
+    }
+    @Test
+    void updateAddressBookDetailsByIdTest() throws Exception {
+        AddressBookDto addressBookDto = new AddressBookDto();
+        addressBookDto.setCity("Srikalahasti");
+        addressBookDto.setFirstName("Vamsi");
+        addressBookDto.setLastName("Krishna");
+        addressBookDto.setState("AndhraPradesh");
+        addressBookDto.setPhoneNumber("9876543210");
+        addressBookDto.setZipCode("654321");
+        int id = 1;
+
+        when(addressBookService.updateAddressBookDetails(id, addressBookDto))
+                .thenReturn(new ResponseDto("DETAILS UPDATED SUCCESSFULLY", HttpStatus.ACCEPTED));
+        mockMvc.perform(MockMvcRequestBuilders.put("/addressbook/details/{Id}",id)
+                .content("{\"id\": 1, \"city\": \"SriKalahasti\",\"firstName\": \"Vamsi\", \"lastName\": \"Krishna\", \"state\": \"AndhraPradesh\", \"phoneNumber\": \"9876543210\", \"zipCode\": \"654321\"}")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is5xxServerError());
+
+    }
+    @Test
+    void deleteAddressBookDetailsTest() throws Exception {
+        when(addressBookService.deleteAddressBookDetails(1)).thenReturn(new ResponseDto("AddressBook details with ID:\" +id+\" Deleted Successfully", HttpStatus.ACCEPTED) );
+        mockMvc.perform(MockMvcRequestBuilders.delete("/addressbook/details/{Id}", 1))
                 .andExpect(status().is5xxServerError());
     }
 }
