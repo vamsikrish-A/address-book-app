03756937a8b7c80609af622ab44451b67dbc9089
vamsikrish-A <krishnavamsi165@gmail.com>
2021-12-17 22:46:02 +0530

    modified:variables with validations in entity

--- src/main/java/com/bridgelabz/addressbookapp/entity/AddressBookEntity.java
+++ src/main/java/com/bridgelabz/addressbookapp/entity/AddressBookEntity.java
@@ -12,9 +12,9 @@ public class AddressBookEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+    private String city;
     private String firstName;
     private String lastName;
-    private String city;
     private String state;
     private String phoneNumber;
     private String zipCode;
